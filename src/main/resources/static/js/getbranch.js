f7c719a224d8d80f243db8d7882aa0f9b533367f
min950903 <min950903@gmail.com>
2020-07-08 01:26:01 +0900

    fix draw timeTable

--- src/main/resources/static/js/getbranch.js
+++ src/main/resources/static/js/getbranch.js
@@ -3,7 +3,7 @@ var userId = $('#user').val();
 
 function drawBranchList() {
 	branchNo = localStorage.getItem('branchNo');
-	var getBranchList = $.ajax({
+	$.ajax({
 			url: '/branch',
 			type:'GET',
 			data: {
@@ -11,37 +11,39 @@ function drawBranchList() {
 				approvalStatus: 'Y'
 			},
 			dataType: 'JSON',
-			contentType : 'application/json;charset=UTF-8'
-	});
-	
-	$.when(getBranchList).done(function(response) {
-		var html="";
-		html +="<select class='form-control ml-4' id='selectBranch' name='selectBranch'>";
-		
-		if(response.length > 0) {
-			for(var i = 0; i < response.length; i++) {
-				if(branchNo > 0) {
-					if(branchNo == response[i].no) {
-						html += "<option value='" + response[i].no +"' selected>" + response[i].name + "</option>";
-					} else {
-						html += "<option value='" + response[i].no +"'>" + response[i].name + "</option>";
-					}
-				} else {
-					if(i == 0) {
-						html += "<option value='" + response[i].no +"' 'selected'>" + response[i].name + "</option>";
-						branchNo = response[i].no;
-					} else {
-						html += "<option value='" + response[i].no +"'>" + response[i].name + "</option>";
+			contentType : 'application/json;charset=UTF-8',
+			success: function(response) {
+				var html="";
+				html +="<select class='form-control ml-4' id='selectBranch' name='selectBranch'>";
+				
+				if(response.length > 0) {
+					for(var i = 0; i < response.length; i++) {
+						if(branchNo > 0) {
+							if(branchNo == response[i].no) {
+								html += "<option value='" + response[i].no +"' selected>" + response[i].name + "</option>";
+							} else {
+								html += "<option value='" + response[i].no +"'>" + response[i].name + "</option>";
+							}
+						} else {
+							if(i == 0) {
+								html += "<option value='" + response[i].no +"' 'selected'>" + response[i].name + "</option>";
+								branchNo = response[i].no;
+							} else {
+								html += "<option value='" + response[i].no +"'>" + response[i].name + "</option>";
+							}
+						}
 					}
 				}
+				html += "</select>";
+				
+				$('#selectBranchDiv').html(html);
+
+			},
+			error: function(error) {
+				alert("오류가 발생했습니다."); 
 			}
-		}
-		html += "</select>";
-		
-		$('#selectBranchDiv').html(html);
-		
 	});
-	
+
 	return branchNo;
 }
 
